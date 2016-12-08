d751ec2f81d4ed38be87776cc7000538fcbe0455
joemerson <santosjoemerson@outlook.com>
2016-12-08 13:53:06 +0000

    +deletar cliente

--- restaurante/main.hs
+++ restaurante/main.hs
@@ -26,6 +26,7 @@ mkYesod "Pagina" [parseRoutes|
 /cliente/cadastrar                CadastroCliente POST
 /cliente/mostrarTodos             MostrarClientes OPTIONS GET
 /cliente/alterar/#ClientesId      AlterarCliente OPTIONS PUT
+/cliente/deletar/#ClientesId      DeletarCliente OPTIONS DELETE
 |]
 
 instance YesodPersist Pagina where
@@ -72,6 +73,19 @@ putAlterarCliente cid = do
     runDB $ update cid [ClientesNome =. (clientesNome cliente),
                         ClientesTelefone =. (clientesTelefone cliente)]
     sendResponse (object [pack "resp" .= pack "Changed"])
+    
+optionsDeletarCliente :: ClientesId -> Handler ()
+optionsDeletarCliente cid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Access-Control-Allow-Methods" "DELETE, OPTIONS"
+    
+deleteDeletarCliente :: ClientesId -> Handler ()
+deleteDeletarCliente cid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    runDB $ delete cid
+    sendResponse (object [pack "resp".= pack "Deleted"])       
+    
+    ------------------------------------------------------------    
     
 connStr = "dbname=d646s1j3kc48hp host=ec2-54-243-203-143.compute-1.amazonaws.com user=rrwiwpzzopujxv password=SUtpmoQKuaw-kY4XxsRamfoNb1 port=5432"
 
