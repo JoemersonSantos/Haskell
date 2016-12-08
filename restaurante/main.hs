d8fa89bb39236de58a3f7e78aadbce7ae3f57ff4
joemerson <santosjoemerson@outlook.com>
2016-12-08 13:52:17 +0000

    +alterar cliente

--- restaurante/main.hs
+++ restaurante/main.hs
@@ -25,6 +25,7 @@ share[mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 mkYesod "Pagina" [parseRoutes|
 /cliente/cadastrar                CadastroCliente POST
 /cliente/mostrarTodos             MostrarClientes OPTIONS GET
+/cliente/alterar/#ClientesId      AlterarCliente OPTIONS PUT
 |]
 
 instance YesodPersist Pagina where
@@ -59,6 +60,19 @@ getMostrarClientes = do
     clientes <- runDB $ selectList [] [Asc ClientesNome]
     sendResponse (object["clientes" .= fmap toJSON clientes])
 
+optionsAlterarCliente :: ClientesId -> Handler ()
+optionsAlterarCliente cid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Access-Control-Allow-Methods" "PUT, OPTIONS"
+    
+putAlterarCliente :: ClientesId -> Handler ()
+putAlterarCliente cid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    cliente <- requireJsonBody :: Handler Clientes
+    runDB $ update cid [ClientesNome =. (clientesNome cliente),
+                        ClientesTelefone =. (clientesTelefone cliente)]
+    sendResponse (object [pack "resp" .= pack "Changed"])
+    
 connStr = "dbname=d646s1j3kc48hp host=ec2-54-243-203-143.compute-1.amazonaws.com user=rrwiwpzzopujxv password=SUtpmoQKuaw-kY4XxsRamfoNb1 port=5432"
 
 main::IO()
